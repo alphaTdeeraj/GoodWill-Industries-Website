84dd2c1ccf3e7b9246e7fd052866fb5bc240d4af
deeraj <deerajn04@gmail.com>
2020-06-30 23:37:15 +0530

    design revamped

--- index.js
+++
@@ -1,6 +0,0 @@
-const burger = document.getElementById("burger-container");
-const closeContainer = document.getElementById("close-container");
-
-burger.addEventListener("click", () => {
-  closeContainer.style.opacity = Math.abs(closeContainer.style.opacity - 1);
-});
