91db79404313d83f0d9ff4609ec2484eb9554da7
deeraj <deerajn04@gmail.com>
2020-07-11 11:18:52 +0530

    added the redirect to calculation and removed the two slide of yawei and rearragned the ordering of the services

---
+++ redirect.js
@@ -0,0 +1,8 @@
+let currentURL = window.location.href;
+const lastURL = currentURL.split("#")[1];
+
+if (lastURL === "calculations") {
+  window.location.replace(
+    "https://alphatdeeraj.github.io/Goodwill-Industries-software/"
+  );
+}
